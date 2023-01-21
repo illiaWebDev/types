8aeedc612a6da15e5de04c60b03f12d121229984
Illia <is@rost.company>
2023-01-21 13:08:09 +0200

    trying good old reexport but without main field in package json

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './core';
+export * as types from './types';
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export * as I18n from './I18n';
+export * as ISO8601 from './ISO8601';
+export * from './Time';
+export * from './GenRtrn';
+export * from './Identity';
+export * from './MapContents';
+export * from './Natural';
+export * from './ObjKeys';
+export * from './Omit';
+export * from './Pick';
+export * from './RecordValues';
