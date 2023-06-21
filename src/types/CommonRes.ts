bb9403130ebd8f8e28b0b9e701f7eaa79c77a963
Illia <is@rost.company>
2023-06-21 09:50:27 +0300

    commnoRes exclude utils

--- src/types/CommonRes.ts
+++ src/types/CommonRes.ts
@@ -1,15 +1,17 @@
 /**
  * these are named with T to avoid collisions (as\
  * SuccessRes, FailureRes are pretty useful to be\
- * used on their own, but also - to denote that \
- * these are generics)
+ * used on their own), but also - to denote that \
+ * these are generics
  */
 export type TSuccessRes< Data = never > = [Data] extends [never]
   ? { success: true }
   : { success: true; data: Data };
 export const tSuccessRes: TSuccessRes = { success: true };
+export type ExcludeTSuccess< T > = Exclude< T, TSuccessRes >;
 
 export type TFailureRes< Err > = {
   success: false;
   error: Err;
 };
+export type ExcludeTFailure< T > = Exclude< T, TFailureRes< unknown > >;
